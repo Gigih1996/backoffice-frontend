293c78142806da1dc49991e6986cb044e1da4499
Gigih_Satriono <gsatriono@gmail.com>
2024-11-06 14:33:37 +0700

    backoffice-update

---
+++ src/app/body/body.component.ts
@@ -0,0 +1,28 @@
+import { style } from '@angular/animations';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.scss']
+})
+export class BodyComponent {
+
+  @Input() collapsed = false;
+  @Input() screenWidth = 0;
+
+
+  getBodyClass(): any {
+    let styleClass = '';
+
+    if(this.collapsed && this.screenWidth > 768){
+      styleClass = 'body-trimmed';
+    }else if (this.collapsed && this.screenWidth <= 768 && this.screenWidth > 0){
+      styleClass = 'body-md-screen'
+    }
+
+    console.log(styleClass)
+    return styleClass;
+  }
+
+}
